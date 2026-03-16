b878c6a2724323a450112adc71879b18052a58a8
Evelyn Milene <Evelynmilenesilva7@gmail.com>
2026-03-15 22:52:19 -0300

    exercícios 1 ao 6

---
+++ ex1.6.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+    int N1;
+    int N2;
+
+    printf ("Primeiro numero: \n");
+    scanf ("%d", &N1);
+
+    printf ("Segundo numero: \n");
+    scanf ("%d", &N2);
+
+       
+
+}
